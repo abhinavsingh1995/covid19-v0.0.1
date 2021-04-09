5b0fb22811eac8fabc9f7c0a1e859c858f17d4e1
abhinavsingh1995 <abhinavsingh6925@gmail.com>
2021-04-09 15:42:12 +0530

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row mt-5">
+      <div class="col">
+        <h2>Positive</h2>
+        <line-chart v-if="arrPositive.length > 0" :chartData="arrPositive" :options="chartOptions" label="Positive"></line-chart>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import moment from 'moment';
+import LineChart from './components/LineChart.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    LineChart
+  },
+  data() {
+    return {
+      arrPositive: [],
+      arrHospitalized: [],
+      arrInIcu: [],
+      arrOnVentilators: [],
+      arrRecovered: [],
+      arrDeaths: [],
+      chartOptions: 
+      {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+
+  async created() {
+  const {data} = await axios.get("https://covidtracking.com/api/us/daily");
+    // console.log(data);
+
+    data.forEach(d => {
+      const date = moment(d.date, 'YYYYMMDD').format('DDMM');
+      const {
+        death,
+        hospitalizedCurrently,
+        inIcuCurrently,
+        onVentilatorCurrently,
+        positive,
+        recovered
+      } = d;
+
+
+      this.arrDeaths.push({date, total: death});
+      this.arrPositive.push({date, total: positive});
+      this.arrHospitalized.push({date, total: hospitalizedCurrently});
+      this.arrInIcu.push({date, total: inIcuCurrently});
+      this.arrOnVentilators.push({date, total: onVentilatorCurrently});
+      this.arrRecovered.push({date, total: recovered});
+
+      // console.log(this.$data);
+
+    });
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
